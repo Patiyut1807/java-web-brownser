7ace14a5ea2dee150c0263da5dd533113e1c3ccd
Amaz <worachanon226@hotmail.com>
2022-04-19 14:35:02 +0700

    PuNe

--- code/src/app/Main.java
+++ code/src/app/Main.java
@@ -1,5 +1,34 @@
 package app;
 
-public class Main {
-    
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage stage) throws IOException {
+		try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/Scene.fxml"));
+		    Parent root = loader.load();	
+		    Controller controller = loader.getController();
+		    Scene scene = new Scene(root);		
+            
+            stage.setTitle("Test");
+		    stage.setScene(scene);
+		    stage.show();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+		
+	}	
+
+	public static void main(String[] args) {
+		
+		launch(args);
+	}
 }
