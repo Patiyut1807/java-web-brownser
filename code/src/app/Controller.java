00044cf28e4a599d1d561fc22137adcfc5a06ba7
Amaz <worachanon226@hotmail.com>
2022-04-27 17:21:25 +0700

    PuNe Updated

--- code/src/app/Controller.java
+++ code/src/app/Controller.java
@@ -1,169 +1,45 @@
 package app;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.*;
 import java.util.*;
 
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebHistory;
-import javafx.scene.web.WebView;
 
-public class Controller implements Initializable {
-
-    @FXML
-    private WebView webView;
-
-    @FXML
-    TextField textField;
-
-    @FXML
-    Button loadButton;
-
-    @FXML
-    Button backButton;
-
-    @FXML 
-    Button forwardButton;
+public class Controller{
 
     @FXML
     TabPane tabPane;
 
     @FXML
-    Tab tabWebview;
+    Tab addTab;
 
-    private WebEngine engine;
+    public void initialize() {
+        createNewTab();
+    }
+    
+    public void addTabEvent(){
+        tabPane.getSelectionModel().selectedItemProperty().addListener((ov, oldTab, newTab) -> {
+            if (newTab == addTab) {
+                createNewTab();
+            }
+        });
+    }
 
-    private WebHistory history;
-
-    private String homePage;
-
-    private double webZoom;
-
-    private URL url;
-    private URLConnection urlConnection;
-    private InputStream inputStream;
-
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
-
-        engine = webView.getEngine();
-        homePage = "www.google.com";
-        webZoom = 1;
-
-        textField.setText(homePage);
+    public void createNewTab(){
+        Tab new_tab = new Tab("new Tab");
+        AnchorPane anch1;
         try {
-            loadPage();
+            anch1 = FXMLLoader.load(getClass().getResource("Tab.fxml"));
+            new_tab.setContent(anch1);
+            tabPane.getTabs().add(tabPane.getTabs().size()-1, new_tab);
+            tabPane.getSelectionModel().select(new_tab);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    public void loadPage() throws IOException {
-        url = new URL("http://" + textField.getText());
-        engine.load("http://" + textField.getText());
-
-        urlConnection = url.openConnection();
-    }
-
-    public void refreshPage() {
-        engine.reload();
-    }
-
-    public void zoomIn() {
-        webZoom += 0.25;
-        webView.setZoom(webZoom);
-    }
-
-    public void zoomOut() {
-        webZoom -= 0.25;
-        webView.setZoom(webZoom);
-    }
-
-    public void displayHostory() {
-        history = engine.getHistory();
-        ObservableList<WebHistory.Entry> entries = history.getEntries();
-
-        for (WebHistory.Entry entry : entries) {
-            // System.out.println(entry);
-            System.out.println(entry.getUrl() + " " + entry.getLastVisitedDate());
-        }
-    }
-
-    public void back() {
-        history = engine.getHistory();
-        ObservableList<WebHistory.Entry> entries = history.getEntries();
-        if (history.getCurrentIndex() == 0)
-            return;
-        history.go(-1);
-        textField.setText(entries.get(history.getCurrentIndex()).getUrl());
-    }
-
-    public void forward() {
-        history = engine.getHistory();
-        ObservableList<WebHistory.Entry> entries = history.getEntries();
-        if (entries.size() - 1 == history.getCurrentIndex())
-            return;
-        history.go(1);
-        textField.setText(entries.get(history.getCurrentIndex()).getUrl());
-    }
-
-    public void getSourceCode() throws IOException {
-
-        System.out.println("Source Code : " + textField.getText());
-
-        inputStream = urlConnection.getInputStream();
-
-        int input;
-
-        do {
-            input = inputStream.read();
-            if (input != -1) {
-                System.out.print((char) input);
-            }
-            if ((char) input == '>') {
-                System.out.println();
-            }
-        } while (input != -1);
-    }
-
-    private void checkBackForward(){
-        history = engine.getHistory();
-        ObservableList<WebHistory.Entry> entries = history.getEntries();
-        
-        if (history.getCurrentIndex() == 0){
-            backButton.setOpacity(0.5);
-        }
-        else backButton.setOpacity(1);
-
-        if (history.getCurrentIndex() == entries.size() - 1){
-            forwardButton.setOpacity(0.5);
-        }
-        else forwardButton.setOpacity(1); 
-    }
-
-    public void newTabEvent(){
-        try
-        {
-            Tab new_tab = new Tab("new Tab");
-            AnchorPane anch1 = FXMLLoader.load(getClass().getResource("newTabTemplate.fxml"));
-            new_tab.setContent(anch1);
-            tabPane.getTabs().addAll(new_tab);
-            tabPane.getSelectionModel().selectLast();
-        }
-        catch(IOException iex)
-        {
-            
-        }
-        
-    }
-
 }
