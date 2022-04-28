ceb7e90786d328b4bda6e3adfb5ae555d1427273
patiphan01 <86480648+paan1@users.noreply.github.com>
2022-04-28 21:46:18 +0700

    updat

--- code/src/app/Main.java
+++ code/src/app/Main.java
@@ -1,33 +1,11 @@
 package app;
 
-import java.io.IOException;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 
-public class Main extends Application {
 
-	@Override
-	public void start(Stage stage) throws IOException {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene.fxml"));
-			Parent root = loader.load();
-			Controller controller = loader.getController();
-			Scene scene = new Scene(root);
+public class Main  {
 
-			stage.setMaximized(true);
-
-			stage.setTitle("Test");
-			stage.setScene(scene);
-			stage.show();
-		} catch (Exception e) {
-			e.getStackTrace();
-		}
-		//testing
-	}
+	
 	public static void main(String[] args) {
-		launch(args);
+		
 	}
 }
